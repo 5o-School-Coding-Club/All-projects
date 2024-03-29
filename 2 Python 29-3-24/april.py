ebe1efb44d2ae5ce32eb6509280f972d7efcf2ec
ManolisG <137608142+ManolisG11@users.noreply.github.com>
2024-03-29 14:30:46 +0200

    Add files via upload

---
+++ 2 Python 29-3-24/april.py
@@ -0,0 +1,24 @@
+while(True):
+    number1= input("Please enter the first number: ")
+    print("-------------------------------------")
+    number1= int(number1)
+    number2= input("Please enter the second number: ")
+    print("-------------------------------------")
+    number2= int(number2)
+    c= number1 + number2
+    print(c)
+    print("-------------------------------------")
+    d= input("Please enter the third number: ")
+    print("-------------------------------------")
+    d= int(d)
+    e= c ** d
+    print(e)
+    print("-------------------------------------")
+    extra= input("EXTRA NUMBER: ")
+    print("-------------------------------------")
+    extra= int(extra)
+    extra2= input("EXTRA EXTRA NUMBER: ")
+    extra2= int(extra2)
+    exfuct= extra / extra2
+    print(exfuct)
+    print("-------------THE END!----------------")
